42294e298408d31e13d785734d0dbd7c9d091238
Vaishu03 <vaishnaviswarnoj@example.com>
2023-02-01 16:39:25 +0530

    Match Specific Pattern

--- bin.py
+++ bin.py
@@ -1,11 +1,42 @@
-def num(n):
-    if n==0:
-        return 0
-    if n==1:
-        return 2
-    if n == 2:
-        return 3
-    return num(n-1)+num(n-2)
+# def num(n):
+#     if n==0:
+#         return 0
+#     if n==1:
+#         return 2
+#     if n == 2:
+#         return 3
+#     return num(n-1)+num(n-2)
 
-n = int(input())
-print(num(n))
+# n = int(input())
+# print(num(n))
+
+def matchSpecificPattern(words, n, pattern):
+    ans = []
+    for word in words:
+        if len(pattern) == len(word) == 1:
+            ans.append(word)
+            continue
+        if len(word) != len(pattern):
+            continue
+        charmap = {}
+        check = True
+        for i in range(len(word)):
+            if pattern[i] not in charmap.keys() and word[i] not in charmap.values():
+                charmap[pattern[i]] = word[i]
+            
+            else:
+                if pattern[i] not in charmap.keys():
+                    check = False
+                    break
+                if charmap[pattern[i]] != word[i]:
+                    check = False
+                    break
+        if check:
+            ans.append(word)
+    return ' '.join(ans)
+T = int(input())
+for i in range(T):
+	N = int(input())
+	L = input().split()
+	P = input()
+	print(matchSpecificPattern(L, N, P))
